bc770d793dc80605db2138b55a20a9aad7d1a002
Nskalkat <C1035076@newcastle.ac.uk>
2022-02-27 18:42:52 +0000

    added DNA codon Table

---
+++ Protein_Param.py
@@ -0,0 +1,25 @@
+from consensus_seq import consensus_Seq
+
+DNA_codon_table = {
+
+# U             C             A             G
+#U
+'TTT': 'Phe', 'TCT': 'Ser', 'TAT': 'Tyr', 'TGU': 'Cys',
+'TTC': 'Phe', 'TCC': 'Ser', 'TAC': 'Tyr', 'TGC': 'Cys',
+'TTA': 'Leu', 'TCA': 'Ser', 'TAA': '---', 'TGA': '---',
+'TTG': 'Leu', 'TCG': 'Ser', 'TAG': '---', 'TGG': 'Trp',
+#C
+'CTT': 'Leu', 'CCT': 'Pro', 'CAT': 'His', 'CGT': 'Arg',
+'CTC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
+'CTA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
+'CTG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
+#A
+'ATT': 'Ile', 'ACT': 'Thr', 'AAT': 'Asn', 'AGT': 'Ser',
+'ATC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
+'ATA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
+'ATG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
+#G
+'GTT': 'Val', 'GCT': 'Ala', 'GAT': 'Asp', 'GGT': 'Gly',
+'GTC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
+'GTA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
+'GTG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
