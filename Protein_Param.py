bf9324d67c0f26fd3c7ba6d744f1e21e618a8be7
Nskalkat <C1035076@newcastle.ac.uk>
2022-02-28 20:15:12 +0000

    fixed code in dictionary not returning single letter amino acid, and finished code for translate of DNA to protein

--- Protein_Param.py
+++ Protein_Param.py
@@ -6,33 +6,41 @@ class protein_param:
 
     # U             C             A             G
     #U
-    'TTT': 'Phe', 'TCT': 'Ser', 'TAT': 'Tyr', 'TGU': 'Cys',
-    'TTC': 'Phe', 'TCC': 'Ser', 'TAC': 'Tyr', 'TGC': 'Cys',
-    'TTA': 'Leu', 'TCA': 'Ser', 'TAA': 'Stop', 'TGA': 'Stop',
-    'TTG': 'Leu', 'TCG': 'Ser', 'TAG': 'Stop', 'TGG': 'Trp',
+    'TTT': 'F', 'TCT': 'S', 'TAT': 'Y', 'TGT': 'C',
+    'TTC': 'F', 'TCC': 'S', 'TAC': 'Y', 'TGC': 'C',
+    'TTA': 'L', 'TCA': 'S', 'TAA': 'Stop', 'TGA': 'Stop',
+    'TTG': 'L', 'TCG': 'S', 'TAG': 'Stop', 'TGG': 'W',
     #C
-    'CTT': 'Leu', 'CCT': 'Pro', 'CAT': 'His', 'CGT': 'Arg',
-    'CTC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
-    'CTA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
-    'CTG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
+    'CTT': 'L', 'CCT': 'P', 'CAT': 'H', 'CGT': 'R',
+    'CTC': 'L', 'CCC': 'P', 'CAC': 'H', 'CGC': 'R',
+    'CTA': 'L', 'CCA': 'P', 'CAA': 'Q', 'CGA': 'R',
+    'CTG': 'L', 'CCG': 'P', 'CAG': 'Q', 'CGG': 'R',
     #A
-    'ATT': 'Ile', 'ACT': 'Thr', 'AAT': 'Asn', 'AGT': 'Ser',
-    'ATC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
-    'ATA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
-    'ATG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
+    'ATT': 'I', 'ACT': 'T', 'AAT': 'N', 'AGT': 'S',
+    'ATC': 'I', 'ACC': 'T', 'AAC': 'N', 'AGC': 'S',
+    'ATA': 'I', 'ACA': 'T', 'AAA': 'K', 'AGA': 'R',
+    'ATG': 'M', 'ACG': 'T', 'AAG': 'K', 'AGG': 'R',
     #G
-    'GTT': 'Val', 'GCT': 'Ala', 'GAT': 'Asp', 'GGT': 'Gly',
-    'GTC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
-    'GTA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
-    'GTG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
+    'GTT': 'V', 'GCT': 'A', 'GAT': 'D', 'GGT': 'G',
+    'GTC': 'V', 'GCC': 'A', 'GAC': 'D', 'GGC': 'G',
+    'GTA': 'V', 'GCA': 'A', 'GAA': 'E', 'GGA': 'G',
+    'GTG': 'V', 'GCG': 'A', 'GAG': 'E', 'GGG': 'G'
     }
 
     def __init__(self):
         self.seq = consensus_Seq().raise_error()
+        self.codon_table = dict(protein_param.DNA_codon_table)
 
     def translate_seq(self):
+        codon_table = self.codon_table
         trans_seq = Seq(self.seq)
-        final_seq = trans_seq.translate()
-        print(final_seq)
+        protein = ""
+        for i in range(0, len(trans_seq), 3):
+            codon = trans_seq[i:i + 3]
+            protein += codon_table[codon]
+        print(protein)
+
+    #def amino_acid_comp
+
 
 protein_param().translate_seq()
