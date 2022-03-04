fc04ddd080f8dbdd291da6ac674840f1e7fb91b3
Nskalkat <C1035076@newcastle.ac.uk>
2022-03-04 11:49:57 +0000

    added documentation and comments to code

---
+++ Protein_Param_Main.py
@@ -0,0 +1,7 @@
+from Protein_Param import protein_param
+
+
+
+if __name__ == "__main__":
+    'The purpose of this method is to just run the all the code in the previous files'
+    protein_param().write_to_file()
